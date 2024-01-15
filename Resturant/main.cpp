546741f22beeea5251efc9255ced619a2f5baf19
solimanrafi1891 <solimanrafi1891@gmail.com>
2024-01-15 12:01:43 +0600

    1st
    start

---
+++ Resturant/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+
+
+
+
+
+
+    return 0;
+}
